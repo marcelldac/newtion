28a40163a51a74ec7f4ae8f1e6657580c178d049
marcelldac <dacmarcell@gmail.com>
2023-11-21 23:21:30 -0300

    feat: create core style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r  from-pink-300 via-purple-300 to-indigo-400">
+      <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[200px] shadow-sm border-black/20">
+        <aside></aside>
+        <main></main>
+      </div>
+    </div>
   );
 }
 
