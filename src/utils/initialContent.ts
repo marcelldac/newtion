e490cec7631fc8e51945210071104c891f25f6e5
marcelldac <dacmarcell@gmail.com>
2023-11-22 01:38:32 -0300

    feat: add emoji on h1

--- src/utils/initialContent.ts
+++ src/utils/initialContent.ts
@@ -1,6 +1,6 @@
 export const initialContent = `
 <div className="max-w-[700px] mx-auto pt-16 prose">
-<h1>NewTion</h1>
+<h1>NewTion 📚</h1>
 <p>Digite // para abrir a área de modificação.</p>
 <p>Selecione algum texto para abir a área de modificação.</p>
 <b>github@marcelldac</b>
