75c736b18e26c7513cf336da9aa6d02d68e4b08d
marcelldac <dacmarcell@gmail.com>
2023-11-22 00:07:14 -0300

    feat: create initialContent instance

---
+++ src/components/initialContent.ts
@@ -0,0 +1,15 @@
+export const initialContent = `
+<div className="max-w-[700px] mx-auto pt-16 prose">
+<h1>Meu Notion</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quis
+  atque deleniti cumque modi rem consequuntur, esse nesciunt debitis natus
+  nobis odio nam reiciendis. Similique impedit vero eum obcaecati
+  blanditiis?
+</p>
+<code>const num = 1;</code>
+<p>
+  You can learn about this at <a href="#">here</a>
+</p>
+</div>
+`;
