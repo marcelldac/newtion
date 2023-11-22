830f7ff185c3efbefc817177c73b876713506fed
marcelldac <dacmarcell@gmail.com>
2023-11-22 01:34:49 -0300

    refactor: move initialContet to utils

---
+++ src/utils/initialContent.ts
@@ -0,0 +1,8 @@
+export const initialContent = `
+<div className="max-w-[700px] mx-auto pt-16 prose">
+<h1>NewTion</h1>
+<p>Digite // para abrir a área de modificação.</p>
+<p>Selecione algum texto para abir a área de modificação.</p>
+<b>github@marcelldac</b>
+</div>
+`;
