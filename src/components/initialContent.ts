a2e8c755e7ff563f973c17b723e7c4d788a86dcf
marcelldac <dacmarcell@gmail.com>
2023-11-22 00:31:26 -0300

    style: removes whitespaces

--- src/components/initialContent.ts
+++ src/components/initialContent.ts
@@ -1,15 +1,8 @@
 export const initialContent = `
 <div className="max-w-[700px] mx-auto pt-16 prose">
 <h1>Meu Notion</h1>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quis
-  atque deleniti cumque modi rem consequuntur, esse nesciunt debitis natus
-  nobis odio nam reiciendis. Similique impedit vero eum obcaecati
-  blanditiis?
-</p>
-<code>const num = 1;</code>
-<p>
-  You can learn about this at <a href="#">here</a>
-</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quis atque deleniti cumque modi rem consequuntur, esse nesciunt debitis natus nobis odio nam reiciendis. Similique impedit vero eum obcaecati blanditiis?</p>
+<code class='language-javascript'>console.log("Hello World!")</code>
+<p>You can learn about this at <a href="#">here</a></p>
 </div>
 `;
