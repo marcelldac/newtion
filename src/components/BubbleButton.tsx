882ab7321cc2df0601686b285060ea5238d8b12c
marcelldac <dacmarcell@gmail.com>
2023-11-22 00:54:30 -0300

    feat: condition bubble button style on click

--- src/components/BubbleButton.tsx
+++ src/components/BubbleButton.tsx
@@ -7,7 +7,7 @@ export interface BubbleButtonProps extends ComponentProps<"button"> {
 export default function BubbleButton(props: BubbleButtonProps) {
   return (
     <button
-      className="p-2 text-zinc-400 text-sm flex items-center gap.15 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+      className="p-2 text-zinc-400 text-sm flex items-center gap.15 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-red-400"
       {...props}
     />
   );
