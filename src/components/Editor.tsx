57cea8ec832c571af24fee94e7d20b33a28342b0
marcelldac <dacmarcell@gmail.com>
2023-11-22 00:44:42 -0300

    feat: add icons to bubble menu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,13 @@
 import { EditorContent, useEditor, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+} from "react-icons/rx";
+import BubbleButton from "./BubbleButton";
 
 export default function Editor() {
   const editor = useEditor({
@@ -20,8 +27,22 @@ export default function Editor() {
         className="max-w-[700px] mx-auto pt-16 prose prose-violet"
       />
       {editor && (
-        <BubbleMenu editor={editor}>
-          <button>Bold</button>
+        <BubbleMenu
+          className="bg-zinc-200 shadow-xl border border-zinc-300 shadow-white/20 rounded-lg overflow-hidden flex divide-x divide-x-zinc-600 "
+          editor={editor}
+        >
+          <BubbleButton>
+            <RxFontBold className="w-4 h-4" />
+          </BubbleButton>
+          <BubbleButton>
+            <RxFontItalic className="w-4 h-4" />
+          </BubbleButton>
+          <BubbleButton>
+            <RxStrikethrough className="w-4 h-4" />
+          </BubbleButton>
+          <BubbleButton>
+            <RxCode className="w-4 h-4" />
+          </BubbleButton>
         </BubbleMenu>
       )}
     </>
